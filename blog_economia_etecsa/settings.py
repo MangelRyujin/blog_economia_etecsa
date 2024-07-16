d9764ea231d2e183e2f2acec096d390dc944f065
mangelryujin <mangelryujin@gmail.com>
2024-07-16 14:50:26 -0400

    modified theme button

--- blog_economia_etecsa/settings.py
+++ blog_economia_etecsa/settings.py
@@ -124,9 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'general/static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
